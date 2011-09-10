f732063f1ba32d3c0fb2587bb74e211b3b8289db
Christoffer Lerno <christoffer@aegik.com>
2011-09-10 16:21:50 +0000

    Added array test

---
+++ src/test/org/aegik/transactionality/ArrayTest.java
@@ -0,0 +1,17 @@
+package org.aegik.transactionality;
+
+import junit.framework.TestCase;
+
+/**
+ * Undocumented Class
+ *
+ * @author Christoffer Lerno
+ */
+public class ArrayTest extends TestCase
+{
+    public void testNewArray()
+    {
+        Array.newArray();    
+    }
+
+}
